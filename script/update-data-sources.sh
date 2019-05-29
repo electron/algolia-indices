e22c45e1a7c6382075b2efcfd2079599aba11701
HashimotoYT <hashimoto.stream@gmail.com>
2019-05-29 16:02:08 +0300

    update fetcher script

--- script/update-data-sources.sh
+++ script/update-data-sources.sh
@@ -16,6 +16,9 @@ npm update electron-i18n
 npm update electron-npm-packages
 npm update electron-releases
 
+# Update electron-api.json
+npm run update-electron
+# Buildes the indices
 npm run build
 npm test
 
